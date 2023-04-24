c89882e10bedd68b2ade037821cb516e650258de
permodig05 <modig.par@gmail.com>
2023-04-24 09:01:08 +0200

    edited

---
+++ schlattahotellet/js/copyright.js
@@ -0,0 +1,10 @@
+function getCurrentYear() {
+    return new Date().getFullYear();
+  }
+  
+  function getCopyright(){
+    var year = getCurrentYear();
+    return "Copyright \u00A9 " + year + " All rights reserved.";
+  }
+  
+  
