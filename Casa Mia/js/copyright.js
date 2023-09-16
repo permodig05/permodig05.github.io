916431bdd56897ea62e9e4bb6257a678a8a28798
permodig05 <modig.par@gmail.com>
2023-09-16 16:22:40 +0200

    up
    u

---
+++ Casa Mia/js/copyright.js
@@ -0,0 +1,8 @@
+function getCurrentYear() {
+    return new Date().getFullYear();
+  }
+  
+  function getCopyright(){
+    var year = getCurrentYear();
+    return "Copyright \u00A9 " + year + " All rights reserved.";
+  }
